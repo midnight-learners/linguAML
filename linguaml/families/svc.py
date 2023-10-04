412bd791c7186f3c26c266641704674b2eafabe8
isaac-fei <isaac.f@gofa.co>
2023-10-04 18:36:09 +0800

    edited svc

--- linguaml/families/svc.py
+++ linguaml/families/svc.py
@@ -2,19 +2,25 @@ from sklearn.svm import SVC
 from ..hp import HPConfig, CategoricalHP
 from .base import define_family_type
 
-class SVCKernel(CategoricalHP):
+class Kernel(CategoricalHP):
     
     Linear = "linear"
     Poly = "poly"
     RBF = "rbf"
     Sigmoid = "sigmoid"
 
+class DecisionFunctionShape(CategoricalHP):
+    
+    OVO = "ovo"
+    OVR = "ovr"
+    
 class SVCConfig(HPConfig):
     
     C: float
-    kernel: SVCKernel
+    kernel: Kernel
     gamma: float
     tol: float
+    decision_function_shape: DecisionFunctionShape
 
 SVCFamily = define_family_type(
     name="SVCFamily",
